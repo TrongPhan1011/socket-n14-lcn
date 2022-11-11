f82d2565f96754bc433ce12e541427d0b3432568
TrongPhan1011 <huutrong2k1@gmail.com>
2022-11-11 13:50:35 +0700

    add change mess

--- index.js
+++ index.js
@@ -16,14 +16,13 @@ io.on('connection', (socket) => {
      * Nhận lại user id và lưu thông tin user id và id soket lại
      * Thêm thành công thì gửi đi "getUsersSocket" với thông tin là list socketUser
      */
-
     socket.on('addUserSocket', (userId) => {
         console.log('A User connected');
         socketUsers = addUserSocket(socketUsers, userId, socket.id);
         io.emit('getUsersSocket', socketUsers);
     });
 
-    // disConnect
+    // disconnect
     socket.on('disconnect', () => {
         console.log('A User disconnected');
         socketUsers = removeUserSocket(socketUsers, socket.id);
@@ -33,16 +32,27 @@ io.on('connection', (socket) => {
     socket.on('sendMessage', ({ receiverId, contentMessage }) => {
         const user = getUserSocket(socketUsers, receiverId);
 
-        console.log(contentMessage);
         if (!!user) {
             socket.join(user.userId);
-
             io.to(user.userId).emit('getMessage', contentMessage);
         } else {
             addUserSocket(socketUsers, receiverId, socket.id);
             const newUser = getUserSocket(socketUsers, receiverId);
             socket.join(newUser.userId);
             io.to(newUser.userId).emit('getMessage', contentMessage);
+        }
+    });
+    socket.on('sendMessChange', ({ receiverId, contentMessage }) => {
+        const user = getUserSocket(socketUsers, receiverId);
+
+        if (!!user) {
+            socket.join(user.userId);
+            io.to(user.userId).emit('getMessChange', contentMessage);
+        } else {
+            addUserSocket(socketUsers, receiverId, socket.id);
+            const newUser = getUserSocket(socketUsers, receiverId);
+            socket.join(newUser.userId);
+            io.to(newUser.userId).emit('getMessChange', contentMessage);
         }
     });
 });
