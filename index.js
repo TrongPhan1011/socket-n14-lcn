12d054fbd0f4c727d908796813d24b9ec4e5c5e6
TrongPhan1011 <huutrong2k1@gmail.com>
2022-11-30 14:15:28 +0700

    fix npm start, port env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { addUserSocket, removeUserSocket, getUserSocket } = require('./src/controller/userController');
 
-const io = require('socket.io')(8900, {
+const PORT = process.env.PORT || 8900;
+const io = require('socket.io')(PORT, {
     cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
